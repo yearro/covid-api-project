ecd28b9bd99273861fc4423297082f5d77362656
Yeri <yeri.armenta@gmail.com>
2022-04-26 21:17:28 -0500

    About us section

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ const Footer = () => (
   <footer className='footer'>
   <div className='content has-text-centered'>
     <p>
-      <strong>Covid API</strong> by <strong>Yeri Armenta</strong> 2022
+      <strong>Covid API</strong> by <strong>Yeri Armenta</strong> 2022 <a href='https://yeriarmenta.name/' title='oficial site'> Oficial Site</a>
     </p>
   </div>
 </footer>
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import imgAboutUs from '../assets/about-us.jpeg'
 const AboutUs = () => (
   <div className='columns'>
-    <div className='column is-half'>
-      <p className='bd-notification is-primary'>
-        <code className='html'>is-half</code>
+    <div className='column'>
+      <p className='bd-notification is-primary has-text-centered'>
+        <code className='html'>We are people interested in people's health. Concerned about transparently reporting the current situation regarding Covid 19</code>
+        <div>
+          <img src={imgAboutUs} alt='About us' />
+        </div>
       </p>
-    </div>
-    <div className='column is-half'>
-      
     </div>
   </div>
 )
